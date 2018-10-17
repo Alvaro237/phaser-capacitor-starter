f6da0a00268f4db44853e268d18a652662ddec14
Alvaro237 <alvaro.mars92@gmail.com>
2018-10-17 23:36:14 +0200

    Initial commit
    Starting phaser-capacitor-starter

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# phaser-capacitor-starter
+# Phaser Starter
+
+![Phaser avatar studying](./src/assets/img/study.png)
+
+Minimal starter project for Phaser with Capacitor capabilites.
+
+## Install
+- Clone this repository git clone https://github.com/Alvaro237/phaser-capacitor-starter.git
+- `cd phaser-capacitor-starter`
+- `npm install`
+
+## Development
+- `npm start`
+- Open `http://localhost:8080`
+
+## Build
+- `npm run build`
+- All game files will be in `/dist`
+
+## Android or iOS build
+
+
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import 'phaser';
+import pkg from 'phaser/package.json';
+import introImage from 'assets/img/study.png';
+
+// This is the entry point of your game.
+
+const width = 800;
+const height = 600;
+
+const config = {
+  width,
+  height,
+  type: Phaser.AUTO,
+  scene: { preload, create },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('study', introImage);
+}
+
+function create() {
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const welcomeMessage = `Welcome to Phaser ${pkg.version}`;
+
+  this.add.image(centerX, centerY * 1.2, 'study');
+
+  this.add
+    .text(centerX, centerY * 0.8, welcomeMessage, { font: "bold 19px Arial", fill: "#fff" })
+    .setOrigin(0.5, 0.5);
+}
